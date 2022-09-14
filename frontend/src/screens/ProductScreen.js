aec3d19819a1791cc0a4f038645d98f7df78702b
Sayak <sayakmukherjee2001@gmail.com>
2022-09-14 10:58:42 +0530

    seventh-commit

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useReducer } from "react";
+import { useContext, useEffect, useReducer } from "react";
 import {useParams} from "react-router-dom";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -10,6 +10,10 @@ import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
 import {Helmet} from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { getError } from "../util";
+import { Store } from "../Store";
 const reducer=(state,action)=>{
     switch(action.type){
       case 'FETCH_REQUEST':
@@ -38,16 +42,20 @@ function ProductScreen(){
             const result = await axios.get(`/api/products/slug/${slug}`);
             dispatch({type: 'FETCH_SUCCESS',payload:result.data});
           }catch(err){
-            dispatch({type:'FETCH_FAIL',payload: error.message});
+            dispatch({type:'FETCH_FAIL',payload: getError(err)});
           }
           
           //setProducts(result.data);
          };
          fetchData();
       },[slug]);
+      const {state,dispatch: cxtDispatch}=useContext(Store);
+      const addToCartHandler =()=>{
+        cxtDispatch({type: 'CART_ADD_ITEM',payload:{...product,quantity:1}})
+      }
     return (
-        loading? <div> Loading...</div>:
-        error? <div> {error}</div>:
+      loading?(<LoadingBox/>):
+      error?(<MessageBox variant="danger">{error}</MessageBox>):
         <div>
             <Row>
                 <Col md={6}>
@@ -94,7 +102,7 @@ function ProductScreen(){
                         {product.countInStock>0 && (
                           <ListGroup.Item>
                           <div className="d-grid">
-                            <Button variant="primary">
+                            <Button onClick={addToCartHandler} variant="primary">
                               Add to Cart
                             </Button>
                           </div>
